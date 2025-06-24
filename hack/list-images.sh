f891d0bee65367995a4d2a4591e83d7161b110f9
Timofei Larkin <lllamnyp@gmail.com>
2025-06-24 17:47:11 +0300

    Add exec bit to script, sanitize image list
    Signed-off-by: Timofei Larkin <lllamnyp@gmail.com>

--- hack/list-images.sh
+++ hack/list-images.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 for node in 11 12 13; do
-  talosctl -n 192.168.123.${node} -e 192.168.123.${node} images ls >> ./hosttmp/images-${SANDBOX_NAME}.txt
-  talosctl -n 192.168.123.${node} -e 192.168.123.${node} images --namespace system ls >> ./hosttmp/images-${SANDBOX_NAME}.txt
+  talosctl -n 192.168.123.${node} -e 192.168.123.${node} images ls >> /workspace/images.tmp
+  talosctl -n 192.168.123.${node} -e 192.168.123.${node} images --namespace system ls >> /workspace/images.tmp
 done
+
+while read _ name sha _ ; do echo $sha $name ; done < /workspace/images.tmp | sort -u > /workspace/images.txt
