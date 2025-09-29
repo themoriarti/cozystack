bb9db7fcaff153f6ff8dff4ce014cac7f5871fcd
Andrei Kvapil <kvapss@gmail.com>
2025-09-29 08:10:02 +0200

    Fix migration to v0.37.0
    Signed-off-by: Andrei Kvapil <kvapss@gmail.com>

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -89,8 +89,8 @@ kubectl get hr -A -o go-template='{{ range .items }}{{ if .spec.suspend }}{{ .sp
   esac
 done
 
-# TODO: Update all Cozystack managed charts to latest version
-# kubectl get hr -A -l cozystack.io/ui=true --no-headers | awk '{print "kubectl patch helmrelease -n " $1 " " $2 " --type=merge -p '\''{\"spec\":{\"chart\":{\"spec\":{\"version\":\"*\"}}}}'\'' "}' | sh -x
+# Update all Cozystack managed charts to latest version
+kubectl get hr -A -l cozystack.io/ui=true --no-headers | awk '{print "kubectl patch helmrelease -n " $1 " " $2 " --type=merge -p '\''{\"spec\":{\"chart\":{\"spec\":{\"version\":\">= 0.0.0-0\"}}}}'\'' "}' | sh -x
 
 # Reconcile platform chart
 trap 'exit' INT TERM
